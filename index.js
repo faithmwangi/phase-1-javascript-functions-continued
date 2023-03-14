94abe7aff18a8c139343aac0b365d15348880c44
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-15 02:26:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+
+ function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+
+ }
+
+ //1) function exists
+ //2) when initialized with '*' creates a function 
+ //that, when called, wraps an adjective in a highlight
+ //3) when initialized with '||' creates a function 
+ //that, when called, wraps an adjective in a highlight
+ function wrapAdjective(star = "*"){
+    return function(adjective= "special"){
+        return `You are ${star}${adjective}${star}!`;}
+ }
